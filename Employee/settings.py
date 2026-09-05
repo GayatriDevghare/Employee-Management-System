5618e72798ebb677c07b0b55870c7dcce9a37eb9
GayatriDevghare <gaytridevghare@gmail.com>
2026-09-05 15:40:17 +0530

    Fix Railway allowed hosts

--- Employee/settings.py
+++ Employee/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
+    "127.0.0.1,localhost,employee-management-system-production-8f70.up.railway.app"
 ).split(",")
 
 
